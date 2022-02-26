380165bdde509d01de555ebd7ff0511483ee4815
bobding <bobding@msn.com>
2022-02-26 09:00:44 -0800

    iris worked

---
+++ build/offb/CMakeFiles/offb_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/demo/catkin_ws/devel/lib/offb/offb_node"
+  "/home/demo/catkin_ws/devel/lib/offb/offb_node.pdb"
+  "CMakeFiles/offb_node.dir/src/offb_node.cpp.o"
+  "CMakeFiles/offb_node.dir/src/offb_node.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offb_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
